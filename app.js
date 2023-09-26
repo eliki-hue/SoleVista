55fed8228fa748ea1e68ec32c31f62e2318b9293
ELIJAH KIRAGU <eliki13720@gmail.com>
2023-09-26 11:04:10 +0300

    created the item display and cart card

---
+++ app.js
@@ -0,0 +1,122 @@
+let list = document.querySelector('.list');
+let cart = document.querySelector('cart-card')
+
+
+
+let products = [
+    {
+        id:1,
+        name :'product name 1',
+        image:'bulb.jpeg',
+        price:1000
+    },
+    {
+        id:3,
+        name :'product name 3',
+        image:'comp.jpeg',
+        price:3000
+    },
+    {
+        id:1,
+        name :'product name 1',
+        image:'bulb.jpeg',
+        price:1000
+    },
+    {
+        id:3,
+        name :'product name 3',
+        image:'comp.jpeg',
+        price:3000
+    },
+    {
+        id:4,
+        name :'product name 4',
+        image:'scroll.jpeg',
+        price:4000
+    },
+    {
+        id:1,
+        name :'product name 1',
+        image:'bulb.jpeg',
+        price:1000
+    },
+    {
+        id:3,
+        name :'product name 3',
+        image:'comp.jpeg',
+        price:3000
+    },
+    {
+        id:1,
+        name :'product name 1',
+        image:'bulb.jpeg',
+        price:1000
+    },
+    {
+        id:3,
+        name :'product name 3',
+        image:'comp.jpeg',
+        price:3000
+    },
+    {
+        id:3,
+        name :'product name 3',
+        image:'comp.jpeg',
+        price:3000
+    },
+    {
+        id:4,
+        name :'product name 4',
+        image:'scroll.jpeg',
+        price:4000
+    },
+    {
+        id:1,
+        name :'product name 1',
+        image:'bulb.jpeg',
+        price:1000
+    },
+    {
+        id:3,
+        name :'product name 3',
+        image:'comp.jpeg',
+        price:3000
+    },
+    {
+        id:1,
+        name :'product name 1',
+        image:'bulb.jpeg',
+        price:1000
+    },
+    {
+        id:3,
+        name :'product name 3',
+        image:'comp.jpeg',
+        price:3000
+    },
+]
+
+function addProduct(){
+    products.forEach((value, key)=>{
+        let newDiv =document.createElement('div')
+        newDiv.classList.add('item');
+        newDiv.innerHTML=`
+        <img src='images/${value.image}'/>
+        <div class="title">${value.name}</div>
+        <div class="price">Price: Ksh. ${value.price.toLocaleString()}</div>
+        <div class="card-body">
+            <button onclick="addToCart(${key})">Add to Cart</button>
+        </div>
+        `;
+        list.appendChild(newDiv);
+    })
+}
+// let newChild =`{
+//     id:4,
+//     name :'product name 4',
+//     image:'scroll.jpeg',
+//     price:4000
+// }`
+// list.appendChild(newChild)
+addProduct()
+
